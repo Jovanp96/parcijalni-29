dfbced6f212390093623489756b56128fff55914
unknown <you@example.com>
2022-05-24 12:46:08 +0200

    idk

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-
+import Nase_meso from './Components/Nase_meso';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div>
-      
+      <Header/>
+      <Nase_meso/>
     </div>
   );
 }
---
+++ Components/Nase_meso.js
@@ -0,0 +1,9 @@
+import './Nase_meso.css'
+
+const Nase_meso =()=> {
+  return (
+    
+  )
+}
+
+export default Nase_meso;
