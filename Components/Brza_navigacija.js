0bc7d703c0a773fef928eee4ffcf48ddfc7a9723
unknown <you@example.com>
2022-05-25 22:56:21 +0200

    started work on Brza navigacija component

---
+++ Components/Brza_navigacija.js
@@ -0,0 +1,22 @@
+import './Brza_navigacija.css'
+
+const Brza_navigacija =()=> {
+  return (
+    <div className='navigacija-main'>
+      <h2>BRZA NAVIGACIJA</h2>
+      <div className='navigacija-sadrzaj'>
+        <div className='navigacija-mesara'>
+          <h3>MESARA BAJAGIĆ</h3>
+          <hr></hr>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit sed ullamcorper morbi tincidunt ornare massa eget. Arcu ac tortor dignissim convallis aenean. Pharetra diam sit amet nisl suscipit.</p>
+          <p>Futoški put BB</p>
+          <p>21000 Novi Sad</p>
+        </div>
+        <div className='navigacija-linkovi'></div>
+        <div className='navigacija-mapa'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Brza_navigacija;
