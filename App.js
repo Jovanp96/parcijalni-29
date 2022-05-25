6d6cdbc34c7323cbdf233434d179d6860f6591eb
unknown <you@example.com>
2022-05-25 22:18:16 +0200

    added navigacija component to app.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import Nase_meso from './Components/Nase_meso';
 import './App.css';
 import Header from './Components/Header';
+import Brza_navigacija from './Components/Brza_navigacija';
 
 function App() {
   return (
     <div>
       <Header/>
       <Nase_meso/>
+      <Brza_navigacija/>
     </div>
   );
 }
