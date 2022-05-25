dfa1425ba12129ad27db855d8812e66940dade31
unknown <you@example.com>
2022-05-25 22:13:46 +0200

    finished nase meso component

--- Components/Nase_meso.js
+++ Components/Nase_meso.js
@@ -17,8 +17,8 @@ const Nase_meso =()=> {
       <div className='nase_meso-item'>
         <img className='heart' src={Heart}></img>
         <div className='nase_meso-item-text'>
-          <h3>NAJKVALITETNIJE</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+          <h3 className='left-text'>NAJKVALITETNIJE</h3>
+          <p className='left-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
         </div>
       </div>
       <div className='nase_meso-item'>
